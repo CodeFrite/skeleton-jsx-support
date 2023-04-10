8f12bd99027f5bffcd38ddd302e5a96c02bf98e8
codeFrite <your_email@example.com>
2023-04-10 13:04:30 +0200

    Revert "Inital commit"
    This reverts commit dc5d9bffc236eca4de9ca684ab85cde9fd7c3a94.

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-/** @jsx shakaJsx */
-declare const shakaJsx: (type: any, props: any, ...children: any[]) => any;
-declare const addChild: (parent: HTMLElement, child: HTMLElement) => void;
-declare const mireba: HTMLElement;
-declare const elem: any;

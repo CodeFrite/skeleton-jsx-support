dc5d9bffc236eca4de9ca684ab85cde9fd7c3a94
codeFrite <your_email@example.com>
2023-04-10 13:03:58 +0200

    Inital commit

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+/** @jsx shakaJsx */
+declare const shakaJsx: (type: any, props: any, ...children: any[]) => any;
+declare const addChild: (parent: HTMLElement, child: HTMLElement) => void;
+declare const mireba: HTMLElement;
+declare const elem: any;
